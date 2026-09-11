460872e3ab4f025a86fa21d81fb6638bdc1068ac
Prachi <prachikukadiya2008@gmail.com>
2026-09-11 16:33:47 +0530

    Add files via upload

---
+++ Python/02_print_variable/01_variable.py
@@ -0,0 +1,7 @@
+name = input("Enter Your Name :-")
+
+print("name", name)
+
+age = input("Enter Your Age :-")
+
+print("Your Age is",age)    
---
+++ Python/02_print_variable/datatypes_2.py
@@ -0,0 +1,66 @@
+# string
+color ="blue"
+
+city ="bhavnagar"
+print("color:",color)
+print("color",type(color))
+print("city:",city)
+print("city",type(city))
+
+
+#integer
+
+age = 18
+print("age:",age)
+print("age",type(age))
+
+marks = 95
+print("marks:",marks)
+print("marks",type(marks))
+
+
+#float
+
+percentage = 94.71
+print("percentage:",percentage)
+
+percentile = 99.46
+print("percentile",percentile)
+
+
+#boolean
+
+is_student = True
+print("is user student:", is_student)
+
+is_married = False
+print("is user married:", is_married)
+
+
+#list
+
+colors = ["black","pink","blue","white"]
+print("colors:",colors)
+
+subjects = ["maths","english","science","social"]
+print("subjects:",subjects)
+
+
+#tuple
+
+colors = ["black","pink","blue","white"]
+print("colors before:",colors)
+
+colors[0]="brown"
+
+print("colors after:",colors)
+
+
+#set
+
+subjects_2 = {"maths","english","science","social","maths"}
+print("subjects_2:",subjects_2)
+
+
+#dictionary
+
